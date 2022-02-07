fca4d3f462b840b81bab66d241ee322113fda962
Mrinal Kumar <kmrinal19@gmail.com>
2022-02-07 18:54:13 +0530

    :hammer: Add model for miscellaneous documents in admin panel

--- stac_portal/stac_application/admin.py
+++ stac_portal/stac_application/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from stac_application.models import Application
+from stac_application.models import Application, MiscellaneousDocument
 
 # Register your models here.
 admin.site.register(Application)
+admin.site.register(MiscellaneousDocument)
